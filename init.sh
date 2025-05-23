eaf36bda2d4d7e5c62aca38b5815b3b87de674b9
Ahmad Hatahet <22600982+ahmadhatahet@users.noreply.github.com>
2025-05-23 16:04:06 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,13 +1,9 @@
-mkdir -p ~/dotfiles
-# mkdir -p ~/.ssh
-
-cp "/mnt/d/ownCloud/dotfiles/aliases" ~/dotfiles/.aliases
-cp "/mnt/d/ownCloud/dotfiles/ssh_config" ~/.ssh/ssh_config
-mv ~/.ssh/ssh_config ~/.ssh/config
+mkdir -p ~/.ssh
+mv ~/dotfiles/ssh_config ~/.ssh/config
 
 cat <<'EOF' >>~/.bashrc
 # add dotfiles
-source ~/dotfiles/.aliases
+source ~/dotfiles/aliases
 EOF
 
 source ~/.bashrc
