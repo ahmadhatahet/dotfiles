a475ae144b528a6cead0807503a7074bb8cb815d
Ahmad Hatahet <22600982+ahmadhatahet@users.noreply.github.com>
2026-04-23 01:13:20 +0200

    Add IdentitiesOnly option for GitHub SSH config

--- generate_ssh_keys_copy_paste.sh
+++ generate_ssh_keys_copy_paste.sh
@@ -25,6 +25,7 @@ Host github.com
     Hostname github.com
     PreferredAuthentications publickey
     IdentityFile ~/.ssh/gh
+    IdentitiesOnly yes
 
 Host gitlab.example.org
     HostName gitlab.example.org
